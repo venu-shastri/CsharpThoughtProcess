5cdfc4c77ecfa677584cf150b1b6d9fe39167ca9
Venugopal  K (C) <vgk@tritechsamvit.com>
2022-08-12 12:08:33 +0530

    Updated

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,7 +10,12 @@ class PatientDataModel
     [ObjectValidationLib.RequiredValidator(ErrorMessage ="MRN Requires Value")]
     
     public string MRN { get; } = Guid.NewGuid().ToString();
+
     [ObjectValidationLib.RequiredValidator(ErrorMessage = "Name Requires Value")]
+    [ObjectValidationLib.LengthValidator(MaxLength=15,ErrorMessage = "Name Requires Value")]
     public string Name { get; set; }
+
+    [[ObjectValidationLib.RangeValidator(1,100,ErrorMessage = "Age Value Must be with in range 1-100")]]
+    public int Age { get; set; }
 }
 
